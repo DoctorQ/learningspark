9f2a9c4765d31746432ebd417056b314c5026a47
qianhui <qianhui_iwm@iwaimai.baidu.com>
2017-07-21 18:45:23 +0800

    pipeline

--- src/main/scala/com/doctorq/spark/ml/CorrelationExample.scala
+++ src/main/scala/com/doctorq/spark/ml/CorrelationExample.scala
@@ -9,12 +9,12 @@ object CorrelationExample {
   def main(args: Array[String]): Unit = {
     val spark = SparkSession.builder.appName("CorrelationExample").getOrCreate()
     spark.sparkContext.setLogLevel("WARN")
-    val data = spark.sparkContext.makeRDD(Seq(
+    val data = Seq(
       Vectors.sparse(4, Seq((0, 1.0), (3, -2.0))),
       Vectors.dense(4.0, 5.0, 0.0, 3.0),
       Vectors.dense(6.0, 7.0, 0.0, 8.0),
       Vectors.sparse(4, Seq((0, 9.0), (3, 1.0)))
-    ))
+    )
     import spark.implicits._
     val df = data.map(Tuple1.apply).toDF("features")
     val Row(coeff1: Matrix) = Correlation.corr(df, "features").head
---
+++ src/main/scala/com/doctorq/spark/ml/HypothesisTestingExample.scala
@@ -0,0 +1,28 @@
+package com.doctorq.spark.ml
+
+import org.apache.spark.ml.linalg.{Vector, Vectors}
+import org.apache.spark.ml.stat.ChiSquareTest
+import org.apache.spark.sql.SparkSession
+
+object HypothesisTestingExample {
+
+  def main(args: Array[String]): Unit = {
+    val spark = SparkSession.builder.appName("HypothesisTestingExample").getOrCreate()
+    spark.sparkContext.setLogLevel("WARN")
+    val data = Seq(
+      (0.0, Vectors.dense(0.5, 10.0)),
+      (0.0, Vectors.dense(1.5, 20.0)),
+      (1.0, Vectors.dense(1.5, 30.0)),
+      (0.0, Vectors.dense(3.5, 30.0)),
+      (0.0, Vectors.dense(3.5, 40.0)),
+      (1.0, Vectors.dense(3.5, 40.0))
+    )
+    import spark.implicits._
+    val df = data.toDF("label", "features")
+    val chi = ChiSquareTest.test(df, "features", "label").head
+    println("pValues = " + chi.getAs[Vector](0))
+    println("degreesOfFreedom = " + chi.getSeq[Int](1).mkString("[", ",", "]"))
+    println("statistics = " + chi.getAs[Vector](2))
+  }
+
+}
