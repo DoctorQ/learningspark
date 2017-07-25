1bf8f4a8d1bf13d88b8816d8ff02e16671d708fa
DoctorQ <542113578@qq.com>
2017-07-25 15:32:39 +0800

    复习TDIDF

---
+++ src/main/scala/com/doctorq/spark/ml/TfIdfExample.scala
@@ -0,0 +1,43 @@
+package com.doctorq.spark.ml
+
+import org.apache.spark.ml.feature.{HashingTF, IDF, Tokenizer}
+import org.apache.spark.sql.SparkSession
+
+object TfIdfExample {
+
+  def main(args: Array[String]): Unit = {
+    val spark = SparkSession.builder().getOrCreate()
+    spark.sparkContext.setLogLevel("WARN")
+    val sentenceData = spark.createDataFrame(Seq(
+      (0.0, "Hi I heard about Spark"),
+      (0.0, "I wish Java could use case classes"),
+      (1.0, "Logistic regression models are neat")
+    )).toDF("label", "sentence")
+
+    //将句子切分为词语
+    val tokenizer = new Tokenizer().setInputCol("sentence").setOutputCol("words")
+    val wordsData = tokenizer.transform(sentenceData)
+    wordsData.show()
+    // 将句子转换为特征向量
+    val hashingTF = new HashingTF()
+      .setInputCol("words").setOutputCol("rawFeatures").setNumFeatures(200)
+
+    val featurizedData = hashingTF.transform(wordsData)
+    featurizedData.show()
+    // alternatively, CountVectorizer can also be used to get term frequency vectors
+    val test = spark.createDataFrame(Seq(
+      (0.0, "numerical measure of how much information a term provides"),
+      (0.0, "information about a particular document. Inverse document "),
+      (1.0, "applied to avoid dividing by zero for terms outside the corpus")
+    )).toDF("label", "sentence")
+    val idf = new IDF().setInputCol("rawFeatures").setOutputCol("features")
+    val idfModel = idf.fit(featurizedData)
+
+    val rescaledData = idfModel.transform(featurizedData)
+    rescaledData.select("features", "label").show()
+    rescaledData.show()
+
+
+  }
+
+}
